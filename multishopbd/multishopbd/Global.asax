e8984a39f5d5393a2b096237c3acdf017c06faed
mahmudulcsejnu <mahmudulcsejnu@gmail.com>
2015-07-20 18:51:03 +0600

    Testing Bootstrap  modal

---
+++ multishopbd/multishopbd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="multishopbd.MvcApplication" Language="C#" %>
---
+++ multishopbd/multishopbd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(multishopbd.Startup))]
+namespace multishopbd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
